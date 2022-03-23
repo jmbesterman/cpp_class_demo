6a6fa6e59771aab86c1b2b1a8ff93b3f99cdfdb3
jmbesterman <jmbesterman@noctrl.edu>
2022-03-23 14:17:28 -0700

    Create Bird.h, Bird.cpp, main.cpp and cMakeLists.txt

---
+++ Bird.cpp
@@ -0,0 +1,5 @@
+// Jeremy Besterman
+// Created by jmbesterman on 3/23/22.
+// Bird class file Bird.cpp
+
+#include "Bird.h"
---
+++ Bird.h
@@ -0,0 +1,14 @@
+// Jeremy Besterman
+// Created by jmbesterman on 3/23/22.
+// Bird header file Bird.h
+
+#ifndef GIT_DEMO_BIRD_H
+#define GIT_DEMO_BIRD_H
+
+
+class Bird {
+
+};
+
+
+#endif //GIT_DEMO_BIRD_H
